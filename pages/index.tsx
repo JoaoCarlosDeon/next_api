1c73fe35fc007960d820d28f9c1ef6d5b8ad63f6
JoaoCarlosDeon <joaocarlosdeon@gmail.com>
2021-09-11 15:28:14 -0300

    Start Projetc - TS AND TAILWIND

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next';
+
+const IndexPage: NextPage = () => { //NEXT PAGE retorna uma ´pagina do NEXT
+  return (
+    <>
+      <h1 className="text-5xl">teste</h1>
+    </>
+  );
+};
+
+export default IndexPage;
